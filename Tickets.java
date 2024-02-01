d52c154fadc8ed4cc18ded025f4dd21cfebada6a
karanam Dhanunjaya <121385905+karanamdhanunjaya@users.noreply.github.com>
2024-02-01 11:13:08 +0530

    import java.util.*; public class sss{     public static void main(String[] args){         Scanner sc= new Scanner(System.in);         int a=sc.nextInt();         if((a*4)>1000) System.out.println("NO");         else System.out.println("YES");     } }

---
+++ Tickets.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class sss{
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        int a=sc.nextInt();
+        if((a*4)>1000) System.out.println("NO");
+        else System.out.println("YES");
+    }
+}
